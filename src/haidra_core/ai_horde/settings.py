c99b79b0f633f8dbbae330235540a81a922c9738
tazlin <tazlin.on.github@gmail.com>
2025-08-12 14:05:51 -0400

    feat: treat urls w/ appropriate pydantic validation type

--- src/haidra_core/ai_horde/settings.py
+++ src/haidra_core/ai_horde/settings.py
@@ -1,9 +1,9 @@
-from pydantic import AliasChoices, Field, SecretStr
+from pydantic import AliasChoices, AnyUrl, Field, HttpUrl, SecretStr
 from pydantic_settings import BaseSettings
 
-DEFAULT_AI_HORDE_URL = "https://aihorde.net/api/"
-DEFAULT_ALT_AI_HORDE_URLS = ["https://stablehorde.net/api/"]
-DEFAULT_RATINGS_URL = "https://ratings.aihorde.net/api/"
+DEFAULT_AI_HORDE_URL = HttpUrl("https://aihorde.net/api/")
+DEFAULT_ALT_AI_HORDE_URLS: list[AnyUrl] = [HttpUrl("https://stablehorde.net/api/")]
+DEFAULT_RATINGS_URL = HttpUrl("https://ratings.aihorde.net/api/")
 
 DEFAULT_ANONYMOUS_API_KEY = "0000000000"
 
@@ -11,11 +11,11 @@ DEFAULT_ANONYMOUS_API_KEY = "0000000000"
 class AIHordeServerSettings(BaseSettings):
     """Base settings for AI Horde server configurations."""
 
-    ai_horde_url: str = Field(
+    ai_horde_url: AnyUrl = Field(
         default=DEFAULT_AI_HORDE_URL,
         validation_alias=AliasChoices("HORDE_URL", "AI_HORDE_URL"),
     )
-    alt_horde_urls: list[str] = Field(default=DEFAULT_ALT_AI_HORDE_URLS)
+    alt_horde_urls: list[AnyUrl] = Field(default=DEFAULT_ALT_AI_HORDE_URLS)
 
 
 class AIHordeClientSettings(BaseSettings):
@@ -24,13 +24,13 @@ class AIHordeClientSettings(BaseSettings):
     api_key: SecretStr = Field(default=SecretStr(DEFAULT_ANONYMOUS_API_KEY))
     """The API key used for authenticating requests to the AI Horde."""
 
-    ai_horde_url: str = DEFAULT_AI_HORDE_URL
+    ai_horde_url: AnyUrl = Field(default=DEFAULT_AI_HORDE_URL)
     """The API endpoint for the AI Horde to interact with."""
 
-    alt_ai_horde_urls: list[str] = Field(default=DEFAULT_ALT_AI_HORDE_URLS)
+    alt_ai_horde_urls: list[AnyUrl] = Field(default=DEFAULT_ALT_AI_HORDE_URLS)
     """Alternative API endpoints for the AI Horde. These should all lead to the same logical AI Horde."""
 
-    ratings_url: str = DEFAULT_RATINGS_URL
+    ratings_url: AnyUrl = Field(default=DEFAULT_RATINGS_URL)
     """The API endpoint for AI Horde ratings."""
 
     logs_folder: str = "./logs"
