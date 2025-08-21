c689e15440fb617f16798d25b221693566cafb66
tazlin <tazlin.on.github@gmail.com>
2025-08-21 11:36:28 -0400

    fix: default no `.env` file; allow unknown env var values

--- src/haidra_core/meta/__init__.py
+++ src/haidra_core/meta/__init__.py
@@ -7,7 +7,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from haidra_core import default_env_files
 
-use_default_env_files = os.environ.get("HAIDRA_USE_DEFAULT_ENV_FILES", "1").lower() not in ["0", "false", ""]
+use_default_env_files = os.environ.get("HAIDRA_USE_DEFAULT_ENV_FILES", "0").lower() not in ["0", "false", ""]
 
 default_settings_config: SettingsConfigDict
 
@@ -15,10 +15,12 @@ if use_default_env_files:
     default_settings_config = SettingsConfigDict(
         env_file=default_env_files,
         use_attribute_docstrings=True,
+        extra="allow",
     )
 else:
     default_settings_config = SettingsConfigDict(
         use_attribute_docstrings=True,
+        extra="allow",
     )
 
 
